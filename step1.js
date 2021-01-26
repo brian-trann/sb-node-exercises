e74b04b0890cbd779f287fd28eefde0d76e930b8
brian-trann <70680670+brian-trann@users.noreply.github.com>
2021-01-26 10:56:56 -0800

    finished step 2, refactor step1

--- step1.js
+++ step1.js
@@ -5,11 +5,13 @@ const file = process.argv[2];
 const cat = (path) => {
 	fs.readFile(path, 'utf-8', (error, data) => {
 		if (error) {
-			console.log(`Error! ${path} : ${error}`);
+			console.error(`Error! ${path} : ${error}`);
 			process.exit(1);
 		} else {
 			console.log(data);
 		}
 	});
 };
-cat(file);
+// cat(file);
+
+module.exports = { cat };
---
+++ step2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+const file = process.argv[2];
+
+const { cat } = require('./step1.js');
+
+// cat(file);
+
+const webCat = async (url) => {
+	try {
+		const response = await axios.get(url);
+		console.log(response);
+	} catch (error) {
+		console.error(`Error. URL: ${url} ; Error: ${error}`);
+		process.exit(1);
+	}
+};
+if (file.slice(0, 4) === 'http') {
+	webCat(file);
+} else {
+	cat(file);
+}
+
+module.exports = { webCat };
